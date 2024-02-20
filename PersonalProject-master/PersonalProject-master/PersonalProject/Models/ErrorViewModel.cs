9b59cd733d5de14ec7125f539cd07a8c7a122b44
Tiana <91290176+TianaKLarmond@users.noreply.github.com>
2024-02-20 00:39:23 -0500

    Delete PersonalProject-master/PersonalProject-master directory

--- PersonalProject-master/PersonalProject-master/PersonalProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PersonalProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
